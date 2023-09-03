d0834e5c2687cbce61599c0686788aeeb77861d2
InvalidInterrupt <InvalidInterrupt@users.noreply.github.com>
2023-09-02 18:48:39 -0700

    Add test for inherited UNIX sockets

--- setup.py
+++ setup.py
@@ -25,7 +25,15 @@ setup(
     install_requires=["twisted[tls]>=22.4", "autobahn>=22.4.2", "asgiref>=3.5.2,<4"],
     python_requires=">=3.7",
     setup_requires=["pytest-runner"],
-    extras_require={"tests": ["hypothesis", "pytest", "pytest-asyncio", "django"]},
+    extras_require={
+        "tests": [
+            "httpunixsocketconnection",
+            "hypothesis",
+            "pytest",
+            "pytest-asyncio",
+            "django",
+        ]
+    },
     entry_points={
         "console_scripts": ["daphne = daphne.cli:CommandLineInterface.entrypoint"]
     },
